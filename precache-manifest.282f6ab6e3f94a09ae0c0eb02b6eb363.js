a7b861ddd8060de6da7937365cac09958c4bc64e
Goncalo <goncalo.magno@gmail.com>
2020-05-11 14:31:37 -0300

    Updates

---
+++ precache-manifest.282f6ab6e3f94a09ae0c0eb02b6eb363.js
@@ -0,0 +1,450 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12a12f49bd358465a3c179f48c55ead",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "969fd1fe533889a73715",
+    "url": "/covid19/static/css/2.f7219a42.chunk.css"
+  },
+  {
+    "revision": "d67cff4adddfb75f6c78",
+    "url": "/covid19/static/css/main.08a370ed.chunk.css"
+  },
+  {
+    "revision": "969fd1fe533889a73715",
+    "url": "/covid19/static/js/2.3bce32b1.chunk.js"
+  },
+  {
+    "revision": "0c0bc3db8d1c99c05e9223d7cbf9603e",
+    "url": "/covid19/static/js/2.3bce32b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d67cff4adddfb75f6c78",
+    "url": "/covid19/static/js/main.c511ded3.chunk.js"
+  },
+  {
+    "revision": "080958ad2f3d2ab682fd",
+    "url": "/covid19/static/js/runtime-main.6dbe199c.js"
+  },
+  {
+    "revision": "9ee4be43ec572ecc1210c540cff40471",
+    "url": "/covid19/static/media/Hermit-Bold.9ee4be43.woff2"
+  },
+  {
+    "revision": "9423a7f2eed82fc0c7c4580e782904bd",
+    "url": "/covid19/static/media/Hermit-BoldItalic.9423a7f2.woff2"
+  },
+  {
+    "revision": "635e121c61e6b2ae1bd2117a9e049b7b",
+    "url": "/covid19/static/media/Hermit-Light.635e121c.woff2"
+  },
+  {
+    "revision": "eb890239ffe31ced209a262723e98c75",
+    "url": "/covid19/static/media/Hermit-LightItalic.eb890239.woff2"
+  },
+  {
+    "revision": "d74c3617da48a886e17ecdd3c68548b8",
+    "url": "/covid19/static/media/Hermit-Regular.d74c3617.woff2"
+  },
+  {
+    "revision": "c6a0928d171eef9e74805dee01b4f587",
+    "url": "/covid19/static/media/Hermit-RegularItalic.c6a0928d.woff2"
+  },
+  {
+    "revision": "8f4b82aa8332273e223a479c8ed512d7",
+    "url": "/covid19/static/media/af.8f4b82aa.png"
+  },
+  {
+    "revision": "913971256fc6daeefc81eb97c4849f3f",
+    "url": "/covid19/static/media/aq.91397125.png"
+  },
+  {
+    "revision": "1d153b2310b64f40acc701b0cfbe5f99",
+    "url": "/covid19/static/media/bl.1d153b23.png"
+  },
+  {
+    "revision": "5305595a5d32ec303e290a0a649f4455",
+    "url": "/covid19/static/media/bt.5305595a.png"
+  },
+  {
+    "revision": "85a3d11dbeeafdcbb0e1f5732458e8d9",
+    "url": "/covid19/static/media/bz.85a3d11d.png"
+  },
+  {
+    "revision": "dbb3305d1cb221f3e67e102ff9cfc257",
+    "url": "/covid19/static/media/cdc-k0KRNtqcjfw-unsplash.dbb3305d.jpg"
+  },
+  {
+    "revision": "c40c8877ea895a4a697bf115d98fc6f2",
+    "url": "/covid19/static/media/ec.c40c8877.png"
+  },
+  {
+    "revision": "a75a1026c5a7e7c12287c9a8dd9fa5df",
+    "url": "/covid19/static/media/gb-wls.a75a1026.png"
+  },
+  {
+    "revision": "52c5124f5836b9d50c021e2e241c1e66",
+    "url": "/covid19/static/media/gs.52c5124f.png"
+  },
+  {
+    "revision": "55331127951d00909b6d99368293dd95",
+    "url": "/covid19/static/media/io.55331127.png"
+  },
+  {
+    "revision": "f2e03d664db0c7a3a7fa461ea1c05fda",
+    "url": "/covid19/static/media/mp.f2e03d66.png"
+  },
+  {
+    "revision": "64341c41d83316b6b37a3189878d0aec",
+    "url": "/covid19/static/media/pe.64341c41.png"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/covid19/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/covid19/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/covid19/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/covid19/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/covid19/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/covid19/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/covid19/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/covid19/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/covid19/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/covid19/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/covid19/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/covid19/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/covid19/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/covid19/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/covid19/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/covid19/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/covid19/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/covid19/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/covid19/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/covid19/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/covid19/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/covid19/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/covid19/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/covid19/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "530704a6520fbb9704dffb739d290810",
+    "url": "/covid19/static/media/rs.530704a6.png"
+  },
+  {
+    "revision": "12256dfdc5a6edaea2d0b8914d6e009a",
+    "url": "/covid19/static/media/sm.12256dfd.png"
+  },
+  {
+    "revision": "f4f2ca898742444293f444f139f07f17",
+    "url": "/covid19/static/media/tm.f4f2ca89.png"
+  },
+  {
+    "revision": "3cc8ea113782646f463b48211bc7171a",
+    "url": "/covid19/static/media/undraw_Books_l33t.3cc8ea11.svg"
+  },
+  {
+    "revision": "5529cd968d0e07e74fe63d4a807812d3",
+    "url": "/covid19/static/media/undraw_Map_dark_k9pw.5529cd96.svg"
+  },
+  {
+    "revision": "33db772b5b80e6f8d6100e8e35570a22",
+    "url": "/covid19/static/media/undraw_Meditation_o89g.33db772b.svg"
+  },
+  {
+    "revision": "603d5252e24b9c5772bc2c4b1e640dfa",
+    "url": "/covid19/static/media/undraw_audio_player_yty9.603d5252.svg"
+  },
+  {
+    "revision": "812d2fcb79a12630870ac1846fdde1e7",
+    "url": "/covid19/static/media/undraw_book_lover_mkck.812d2fcb.svg"
+  },
+  {
+    "revision": "9b964ca1babf73ac48b29494388d3a74",
+    "url": "/covid19/static/media/undraw_book_reading_kx9s.9b964ca1.svg"
+  },
+  {
+    "revision": "52529c0b14b1a6278c23d386d810799e",
+    "url": "/covid19/static/media/undraw_cancel_u1it.52529c0b.svg"
+  },
+  {
+    "revision": "2f9d1e244a27e470f0499d5f946b0304",
+    "url": "/covid19/static/media/undraw_conference_call_b0w6.2f9d1e24.svg"
+  },
+  {
+    "revision": "0585e140e352bef59f1e60cd786bc1c5",
+    "url": "/covid19/static/media/undraw_cooking_lyxy.0585e140.svg"
+  },
+  {
+    "revision": "66e903c86923ffdf422ac77ea9e86165",
+    "url": "/covid19/static/media/undraw_cup_of_tea_6nqg.66e903c8.svg"
+  },
+  {
+    "revision": "a2dee86477324016df8f3d21b22649f8",
+    "url": "/covid19/static/media/undraw_dev_productivity_umsq.a2dee864.svg"
+  },
+  {
+    "revision": "45a94407e4c5011b827e102bd3f12304",
+    "url": "/covid19/static/media/undraw_doctors_hwty.45a94407.svg"
+  },
+  {
+    "revision": "5aca547fa998171493ce8fb13d3cfebe",
+    "url": "/covid19/static/media/undraw_dog_walking_0jpt.5aca547f.svg"
+  },
+  {
+    "revision": "7d62e0aa4293f8af5e8baab1286910eb",
+    "url": "/covid19/static/media/undraw_gradma_wanr.7d62e0aa.svg"
+  },
+  {
+    "revision": "175331e4ec3656823e3bcf4daaa2df9e",
+    "url": "/covid19/static/media/undraw_group_hangout_5gmq.175331e4.svg"
+  },
+  {
+    "revision": "7bb256735805af7a788c2ca8b9dbc55d",
+    "url": "/covid19/static/media/undraw_group_video_el8e.7bb25673.svg"
+  },
+  {
+    "revision": "4843a42f3b61dafc65c363ecf0f18ab8",
+    "url": "/covid19/static/media/undraw_healthy_options_sdo3.4843a42f.svg"
+  },
+  {
+    "revision": "daa69ef69da0137de2005ca19d399ccd",
+    "url": "/covid19/static/media/undraw_home_cinema_l7yl.daa69ef6.svg"
+  },
+  {
+    "revision": "2f2fb9e5ad7cf0957df760fcdd26862a",
+    "url": "/covid19/static/media/undraw_indoor_bike_pwa4.2f2fb9e5.svg"
+  },
+  {
+    "revision": "85b21ae0fca06927d65712eb0a078b83",
+    "url": "/covid19/static/media/undraw_love_xfcv.85b21ae0.svg"
+  },
+  {
+    "revision": "5c8ba9424839b9c37f7cff44622db547",
+    "url": "/covid19/static/media/undraw_map_1r69.5c8ba942.svg"
+  },
+  {
+    "revision": "748e9f82200b04b96cb02d33ebb55125",
+    "url": "/covid19/static/media/undraw_mathematics_4otb.748e9f82.svg"
+  },
+  {
+    "revision": "72781949af08172f156f4829818c4ee8",
+    "url": "/covid19/static/media/undraw_medical_care_movn.72781949.svg"
+  },
+  {
+    "revision": "7702c99606558483e89f798e20e0bad2",
+    "url": "/covid19/static/media/undraw_medical_research_qg4d.7702c996.svg"
+  },
+  {
+    "revision": "3d36874b10ce6893beb601298576272f",
+    "url": "/covid19/static/media/undraw_medicine_b1ol.3d36874b.svg"
+  },
+  {
+    "revision": "faf6d6eddcd621ae4923967da193868b",
+    "url": "/covid19/static/media/undraw_meditating_0nae.faf6d6ed.svg"
+  },
+  {
+    "revision": "129ee219744398a721abc410387f3152",
+    "url": "/covid19/static/media/undraw_mindfulness_scgo.129ee219.svg"
+  },
+  {
+    "revision": "37300f74922c8440c3d9f261777035f4",
+    "url": "/covid19/static/media/undraw_mint_tea_7su0.37300f74.svg"
+  },
+  {
+    "revision": "2ae390fd2d45d9faf717da86e3950585",
+    "url": "/covid19/static/media/undraw_neighbors_ciwb.2ae390fd.svg"
+  },
+  {
+    "revision": "9c582e876d3421460bca6582887b0248",
+    "url": "/covid19/static/media/undraw_netflix_q00o.9c582e87.svg"
+  },
+  {
+    "revision": "6c2118fcab2747a48a79ff8b1f714ebe",
+    "url": "/covid19/static/media/undraw_online_articles_79ff.6c2118fc.svg"
+  },
+  {
+    "revision": "78cd4c40b9005ca27a78ad5f028980de",
+    "url": "/covid19/static/media/undraw_online_groceries_a02y.78cd4c40.svg"
+  },
+  {
+    "revision": "e129253f26be679bdf8e0170d7cef4f0",
+    "url": "/covid19/static/media/undraw_online_information_4ui6.e129253f.svg"
+  },
+  {
+    "revision": "93eddb70dfa31c8c4d528547032c5f02",
+    "url": "/covid19/static/media/undraw_online_messaging_9ro6.93eddb70.svg"
+  },
+  {
+    "revision": "339fbbc05ec19c74d8e7220189b717a7",
+    "url": "/covid19/static/media/undraw_online_video_ivvq.339fbbc0.svg"
+  },
+  {
+    "revision": "577eab6a3ecda1954553940134fca049",
+    "url": "/covid19/static/media/undraw_phone_call_grmk.577eab6a.svg"
+  },
+  {
+    "revision": "88daf35dc15db45d898b0f755e6900a3",
+    "url": "/covid19/static/media/undraw_pilates_gpdb.88daf35d.svg"
+  },
+  {
+    "revision": "ab41fb5a2e3f4258246769741ab7ecf3",
+    "url": "/covid19/static/media/undraw_positive_attitude_xaae.ab41fb5a.svg"
+  },
+  {
+    "revision": "0af0c1a84f5c0f167cfa5f2800d7014f",
+    "url": "/covid19/static/media/undraw_quality_time_wiyl.0af0c1a8.svg"
+  },
+  {
+    "revision": "bca8e049a29c57c5d6ba65f651b5ed36",
+    "url": "/covid19/static/media/undraw_quite_town_mg2q.bca8e049.svg"
+  },
+  {
+    "revision": "bbd6f254de01750f2f8fadaceb1ad556",
+    "url": "/covid19/static/media/undraw_reading_list_4boi.bbd6f254.svg"
+  },
+  {
+    "revision": "d3d6eb5b377f8054e337344edb70a12b",
+    "url": "/covid19/static/media/undraw_reading_time_gvg0.d3d6eb5b.svg"
+  },
+  {
+    "revision": "853f2645291cf678560d478d3870cdb5",
+    "url": "/covid19/static/media/undraw_real_time_collaboration_c62i.853f2645.svg"
+  },
+  {
+    "revision": "29ecd97ee1b290936d21d2c7c1c74244",
+    "url": "/covid19/static/media/undraw_relaxing_at_home_9tyc.29ecd97e.svg"
+  },
+  {
+    "revision": "cce762074f8d2a58bff93d8ff274e4c5",
+    "url": "/covid19/static/media/undraw_remotely_2j6y.cce76207.svg"
+  },
+  {
+    "revision": "b1a90be1bd3477b374ce23a1475bdcca",
+    "url": "/covid19/static/media/undraw_researching_22gp.b1a90be1.svg"
+  },
+  {
+    "revision": "6959c266998218325c86bdd26a56a6d1",
+    "url": "/covid19/static/media/undraw_science_fqhl.6959c266.svg"
+  },
+  {
+    "revision": "0e9f72a767b43d664b17aab2f3839585",
+    "url": "/covid19/static/media/undraw_shopping_app_flsj.0e9f72a7.svg"
+  },
+  {
+    "revision": "b2dc530016e0d373de94c0be4c2296d6",
+    "url": "/covid19/static/media/undraw_social_distancing_2g0u.b2dc5300.svg"
+  },
+  {
+    "revision": "d9166e14dd077d1f77e5aa92201050ee",
+    "url": "/covid19/static/media/undraw_stranded_traveler_pdbw.d9166e14.svg"
+  },
+  {
+    "revision": "fb29bba188b46d266c42855968cb6d11",
+    "url": "/covid19/static/media/undraw_video_game_night_8h8m.fb29bba1.svg"
+  },
+  {
+    "revision": "8c121909c25627e426ff7fad694cb8bd",
+    "url": "/covid19/static/media/undraw_wash_hands_nwl2.8c121909.svg"
+  },
+  {
+    "revision": "449c436b04abdf1f2d3777d75432890d",
+    "url": "/covid19/static/media/undraw_web_developer_p3e5.449c436b.svg"
+  },
+  {
+    "revision": "ce9a266f62cb19a76449d4f03b2d1a41",
+    "url": "/covid19/static/media/undraw_web_search_eetr.ce9a266f.svg"
+  },
+  {
+    "revision": "c32e6d1131ff784a79f8e1557ed8852d",
+    "url": "/covid19/static/media/undraw_web_shopping_dd4l.c32e6d11.svg"
+  },
+  {
+    "revision": "7e9cb32d78dc7d99b2b689036aba6a9b",
+    "url": "/covid19/static/media/undraw_working_remotely_jh40.7e9cb32d.svg"
+  },
+  {
+    "revision": "c071d2b77fdc241e7fc9be524bdeba42",
+    "url": "/covid19/static/media/undraw_world_9iqb.c071d2b7.svg"
+  },
+  {
+    "revision": "33ae6c520a6af1e5556fb241c0970c7e",
+    "url": "/covid19/static/media/undraw_yoga_248n.33ae6c52.svg"
+  },
+  {
+    "revision": "487addb0a1edd4a30c17e33cc7117098",
+    "url": "/covid19/static/media/va.487addb0.png"
+  },
+  {
+    "revision": "fd18669807e0a2baa0b4f70ca77a0177",
+    "url": "/covid19/static/media/vi.fd186698.png"
+  }
+]);
